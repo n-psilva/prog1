be13519c4e6dcbc61c706f8b894eff876a98aceb
n-psilva <natanp.silva@hotmail.com>
2017-09-29 19:35:21 -0300

    Add files via upload

---
+++ fibo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int ant,pos,fibo,n,i;
+    ant=0;
+    pos=1;
+    printf("Digite um numero para a ate que termo deve ir a sequencia de Fibonacci:\n\n");
+    scanf("%d",&n);
+    printf("Sequencia = );
+    if(>=1);
+    {
+       printf("0, ");
+    }
+    if(>=2);
+    {
+       printf("1, ");
+    }
+    for(i=3;i<=n;i++)
+    {
+                     fibo=ant+pos;
+                     printf("%d, ",fibo);
+                     ant=pos;
+                     pos=fibo;
+    }
+    system("pause");
+    return 0;
+    
+}
+                     
